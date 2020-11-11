3e615ee90450b4bfa5b2848ac7cda560feace845
99002570 <71587032+99002570@users.noreply.github.com>
2020-11-11 17:16:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from donor import *
-A = blooddb("GBG", "saxaxi", "dlodjc", "spiqsid", "xjnsdkci")
+A = blooddb("GBG", "saxaxi", "geethu", "Grce", "xjnsdkci")
 A.read_data(thelist)
 A.write_data(thelist)
 B = input("Enter phone num to search:")
